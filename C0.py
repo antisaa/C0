9398b7cb87b26b05ecbefad8a1705f9fd870db47
Nikola <nikola.jelic995@gmail.com>
2018-06-15 17:14:29 +0200

    Osnovne operacije.

--- C0.py
+++ C0.py
@@ -1,11 +1,11 @@
 from pj import *
 
 class c0(enum.Enum):
-    PLUS, MINUS, PUTA, KROZ = '+-*/'
+    PLUS, MINUS, PUTA, KROZ, OTV, ZATV = '+-*/()'
 
     class BROJ(Token):
         def vrijednost(self, mem):
-            return mem[self]
+            return int(self.sadržaj)
 
 
 
@@ -27,10 +27,17 @@ def c0_lex(string):
 # baza -> BROJ | OTV izraz ZATV                             KVAČICA
 
 class c0Parser(Parser):
+    def start(self):
+        env = []
+        while True:
+            izraz = self.izraz()
+            if self >> E.KRAJ: return Program(env, izraz)
+            else: self.greška()
+
     def izraz(self):
         trenutni = self.član()
         while True:
-            if self >> {AC.PLUS, AC.MINUS}:
+            if self >> {c0.PLUS, c0.MINUS}:
                 trenutni = Binarna(
                     op = self.zadnji,
                     lijevo = trenutni,
@@ -41,48 +48,53 @@ class c0Parser(Parser):
     def član(self):
         trenutni = self.faktor()
         while True:
-            if self >> {AC.PUTA, AC.KROZ}:
+            if self >> {c0.PUTA, c0.KROZ}:
                 trenutni = Binarna(self.zadnji, trenutni, self.faktor())
             else: return trenutni
 
     def faktor(self):
-        if self >> AC.MINUS: return Unarna(self.zadnji, self.faktor())
+        if self >> c0.MINUS: return Unarna(self.zadnji, self.faktor())
         baza = self.baza()
         return baza
 
     def baza(self):
-        if self >> AC.BROJ: trenutni = self.zadnji
-        elif self >> AC.OTV:
+        if self >> c0.BROJ: trenutni = self.zadnji
+        elif self >> c0.OTV:
             trenutni = self.izraz()
-            self.pročitaj(AC.ZATV)
+            self.pročitaj(c0.ZATV)
         else: self.greška()
         return trenutni
 
-    start = izraz
 
-
+class Program(AST('okolina izraz')):
+    def izvrši(self):
+        env = {}
+        for ime, izraz in self.okolina: env[ime.sadržaj] = izraz.vrijednost(env)
+        return self.izraz.vrijednost(env)
 
 class Binarna(AST('op lijevo desno')):
     def vrijednost(self, env):
         o,x,y = self.op, self.lijevo.vrijednost(env), self.desno.vrijednost(env)
         try:
-            if o ** AC.PLUS: return x + y
-            elif o ** AC.MINUS: return x - y
-            elif o ** AC.PUTA: return x * y
-            elif o ** AC.KROZ: return x / y
+            if o ** c0.PLUS: return x + y
+            elif o ** c0.MINUS: return x - y
+            elif o ** c0.PUTA: return x * y
+            elif o ** c0.KROZ: return x / y
             else: assert not 'slučaj'
         except ArithmeticError as ex: o.problem(*ex.args)
 
 class Unarna(AST('op ispod')):
     def vrijednost(self, env):
         o, z = self.op, self.ispod.vrijednost(env)
-        if o ** AC.MINUS: return -z
+        if o ** c0.MINUS: return -z
 
 
-def izračunaj(string): return ACParser.parsiraj(ac_lex(string)).izvrši()
+def izračunaj(string): return c0Parser.parsiraj(c0_lex(string)).izvrši()
+def tokeni(string): return list(c0_lex(string))
+
 
 if __name__ == '__main__':
-    tokeni = c0_lex('3+7*4')
-    print(list(tokeni))
-    print(izračunaj('3+7*4'))
+
+    print(tokeni('(3+7)*4'))
+    print(izračunaj('(3+7)*4'))
 
