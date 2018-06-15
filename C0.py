26983dd1cc5de8901a916a4e7d14f9153cc50b7e
Nikola <nikola.jelic995@gmail.com>
2018-06-16 00:09:13 +0200

    Modul.

--- C0.py
+++ C0.py
@@ -1,11 +1,28 @@
 from pj import *
 
-class c0(enum.Enum):
-    PLUS, MINUS, PUTA, KROZ, OTV, ZATV = '+-*/()'
+def sign(number):
+    if(number < 0):
+        return -1
+    else:
+        return 1
 
+def modl(number, br):
+    return sign(int(number)) * ( (sign(int(number))*int(number)) % int(br))
+
+def integer(number):
+    number = sign(int(number)) * ( (sign(int(number))*int(number)) % (2**32))
+    if int(number) > 2**31:
+        return int(number) - 2**32
+    else:
+        return number
+
+class c0(enum.Enum):
+    PLUS, MINUS, PUTA, KROZ, MOD = '+-*/%'
+    JEDNAKO, MANJE, OTV, ZATV = '=<()'
+    INT, BOOL = 'int', 'bool'
     class BROJ(Token):
         def vrijednost(self, mem):
-            return int(self.sadržaj)
+            return integer(self.sadržaj)
 
 
 
@@ -20,11 +37,11 @@ def c0_lex(string):
         else: yield lex.token(operator(c0, znak) or lex.greška())
 
 ### Beskontekstna gramatika
-# start ->  izraz                                           KVAČICA
-# izraz -> izraz PLUS član | izraz MINUS član | član        KVAČICA
-# član -> član PUTA faktor | član KROZ faktor | faktor      KVAČICA
-# faktor ->  baza | MINUS faktor                            KVAČICA
-# baza -> BROJ | OTV izraz ZATV                             KVAČICA
+# start ->  izraz                                                           KVAČICA
+# izraz -> izraz PLUS član | izraz MINUS član | član                        KVAČICA
+# član -> član PUTA faktor | član KROZ faktor | član MOD faktor | faktor
+# faktor ->  baza | MINUS faktor                                            KVAČICA
+# baza -> BROJ | OTV izraz ZATV                                             KVAČICA
 
 class c0Parser(Parser):
     def start(self):
@@ -48,7 +65,7 @@ class c0Parser(Parser):
     def član(self):
         trenutni = self.faktor()
         while True:
-            if self >> {c0.PUTA, c0.KROZ}:
+            if self >> {c0.PUTA, c0.KROZ, c0.MOD}:
                 trenutni = Binarna(self.zadnji, trenutni, self.faktor())
             else: return trenutni
 
@@ -76,10 +93,18 @@ class Binarna(AST('op lijevo desno')):
     def vrijednost(self, env):
         o,x,y = self.op, self.lijevo.vrijednost(env), self.desno.vrijednost(env)
         try:
-            if o ** c0.PLUS: return x + y
-            elif o ** c0.MINUS: return x - y
-            elif o ** c0.PUTA: return x * y
-            elif o ** c0.KROZ: return x // y #handlaj  dijeljenje nulom
+            if o ** c0.PLUS: return integer(x + y)
+            elif o ** c0.MINUS: return integer(x - y)
+            elif o ** c0.PUTA: return integer(x * y)
+            elif o ** c0.KROZ:
+                print(x, y)
+                if y == 0 : raise SemantičkaGreška("Dijeljenje s nulom.")
+                elif integer(x) == -2**31 and integer(y) == -1 : raise SemantičkaGreška("Greška: overflow.")
+                return integer(sign(x) * ( (sign(x)*x) // y) ) #handlaj  dijeljenje nulom
+            elif o ** c0.MOD:
+                if y == 0 : raise SemantičkaGreška("Dijeljenje s nulom.")
+                elif integer(x) == -2**31 and integer(y) == -1 : raise SemantičkaGreška("Greška: overflow.")
+                return integer(sign(x) * ( (sign(x)*x) % y) ) #handlaj  dijeljenje nulom
             else: assert not 'slučaj'
         except ArithmeticError as ex: o.problem(*ex.args)
 
@@ -95,6 +120,17 @@ def tokeni(string): return list(c0_lex(string))
 
 if __name__ == '__main__':
 
-    print(tokeni('(3+7)/0'))
-    print(izračunaj('(3+7)/0'))
+    print(tokeni('(3+7)%3'))
+    print(izračunaj('(3+7)%3'))
+    print(integer(2**31+1))
+    print(izračunaj('-37%5'))
+    print(izračunaj('-37/5'))
+    #print(izračunaj('-2147483648/-1'))
+    print(-37 % 5)
+    print(-37 // 5)
+    print(sign(-5))
+    print(-2**31)
+    print(integer(-2**31))
+    print(int(-2**31)%(2**32))
+    print(modl(-37,5))
 
